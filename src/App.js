6f93a85e47bb1897c6e7cb1742e8119168854d52
Gokul <gokulramalingam1@gmail.com>
2019-11-10 22:24:16 +0530

    Update in App component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage';
-
+import Shop from './pages/Shop/Shop'
 function App() {
   return (
     <div>
      <Route exact path="/" component={Homepage}/>
+     <Route exact path="/shop" component={Shop}/>
     </div>
   );
 }
