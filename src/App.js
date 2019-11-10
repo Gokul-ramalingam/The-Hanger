f9390b9c43f39a51d0518caa60d4dda34ce7e115
Gokul <gokulramalingam1@gmail.com>
2019-11-11 00:43:52 +0530

    Update in script

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import { Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage';
 import Shop from './pages/Shop/Shop'
+import Header from '../src/components/header/header'
+
 function App() {
   return (
     <div>
+    <Header />
      <Route exact path="/" component={Homepage}/>
      <Route exact path="/shop" component={Shop}/>
     </div>
