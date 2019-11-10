e04f74801130ede6e274805c8bd125c9fdd9f5bc
Gokul <gokulramalingam1@gmail.com>
2019-11-10 23:32:10 +0530

    Update in component

--- src/components/preview-collection/preview-collection.js
+++ src/components/preview-collection/preview-collection.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import CollectionItem from '../collection-item/collection-item'
+import './preview-collection.scss'
 
 const CollectionPreview = ({ title , items }) => (
     <div className="collection-preview">
     <h1 className="title">{title}</h1>
     <div className="preview">
       { items.filter((item,index)=> index < 4)
-                  .map(item =>(
-           <div key={item.id}>{item.name}</div>
+                  .map(({id,...otherProps}) =>(
+           <CollectionItem key={id} {...otherProps} />
       ))}
     </div>
     </div>
