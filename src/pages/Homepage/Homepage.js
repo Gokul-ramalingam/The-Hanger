c9e1c75e5a7c0c5cfce293f1d175f71e05faae45
Gokul <gokulramalingam1@gmail.com>
2019-11-10 14:27:15 +0530

    Update in homepage jsx

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,40 +1,11 @@
 import React from 'react';
 import './Homepage.scss'
+import Directory from '../../components/directory-menu/directory-menu';
 const Homepage = () =>{
     return(
-        <div className="directory-menu">
-            <div className="menu-item">
-               <div className="content">
-                  <h1 className="title">HATS</h1>
-                  <span className="subtitle">SHOP NOW</span>
-               </div>
-            </div>
-            <div className="menu-item">
-               <div className="content">
-                  <h1 className="title">JACKETS</h1>
-                  <span className="subtitle">SHOP NOW</span>
-               </div>
-            </div>
-            <div className="menu-item">
-            <div className="content">
-               <h1 className="title">SNEAKERS</h1>
-               <span className="subtitle">SHOP NOW</span>
-            </div>
-         </div>
-         <div className="menu-item">
-         <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-         </div>
-      </div>
-      <div className="menu-item">
-      <div className="content">
-         <h1 className="title">MENS</h1>
-         <span className="subtitle">SHOP NOW</span>
-      </div>
-   </div>
-         
-   </div>
+        <div className="homepage">
+            <Directory />
+        </div>
     )
 }
 
