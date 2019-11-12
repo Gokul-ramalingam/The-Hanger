ebd85eb10ba80b1562161e47375b9d7080ca77c6
Gokul <gokulramalingam1@gmail.com>
2019-11-12 12:14:18 +0530

    Update in app component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage';
 import Shop from './pages/Shop/Shop'
 import Header from '../src/components/header/header'
+import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Header />
      <Route exact path="/" component={Homepage}/>
      <Route exact path="/shop" component={Shop}/>
+     <Route exact path="/signin" component={SignInAndSignUp}/>
     </div>
   );
 }
