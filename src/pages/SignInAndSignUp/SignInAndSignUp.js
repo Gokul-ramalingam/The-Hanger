258ba14b9b821dd83470988030d434b29a6622c6
Gokul <gokulramalingam1@gmail.com>
2019-11-12 12:15:24 +0530

    Addition of SignInandSignup page

---
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './SignInAndSignUp.scss'
+import SignIn from '../../components/signIn/signIn'
+
+
+const  SignInAndSignUp = () => (
+    <div className="sign-in-and-sign-up">
+    <SignIn />
+    </div>
+)
+
+
+export default SignInAndSignUp;
+
