f2ae8c0ec8ebfb544e6883714bfe8ee02b6d3df6
Gokul <gokulramalingam1@gmail.com>
2019-11-13 02:29:29 +0530

    Update in App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage';
 import Shop from './pages/Shop/Shop'
 import Header from '../src/components/header/header'
 import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp'
+import { auth } from './firebase/firebase.utils'
+class App extends Component{
 
-function App() {
+  constructor(){
+    super()
+    this.state = {
+       currentUser : null
+    }
+  }
+ unsubsccribeFromAuth = null;
+  componentDidMount(){
+    this.unsubsccribeFromAuth = auth.onAuthStateChanged(user =>{
+      this.setState({
+        currentUser: user
+      })
+      console.log(user);
+    })
+    
+  }
+
+  componentWillUnmount(){
+     this.unsubsccribeFromAuth();
+  }
+
+  render(){
   return (
     <div>
-    <Header />
+    <Header currentUser={this.state.currentUser} /> 
      <Route exact path="/" component={Homepage}/>
      <Route exact path="/shop" component={Shop}/>
      <Route exact path="/signin" component={SignInAndSignUp}/>
     </div>
   );
+}
 }
 
 export default App;
