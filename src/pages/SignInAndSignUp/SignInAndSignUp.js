9ac630dc7bd30db2e66604dd5905ae9cf5ee03e4
Gokul <gokulramalingam1@gmail.com>
2019-11-13 10:05:55 +0530

    changes in signupandsignin page

--- src/pages/SignInAndSignUp/SignInAndSignUp.js
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 import './SignInAndSignUp.scss'
 import SignIn from '../../components/signIn/signIn'
-
+import SignUp from '../../components/signup/signup'
 
 const  SignInAndSignUp = () => (
     <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
     </div>
 )
 
