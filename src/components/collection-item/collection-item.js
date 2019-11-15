d82fe071941772911c32d58ccdcdb9be2a729078
Gokul <gokulramalingam1@gmail.com>
2019-11-16 04:46:09 +0530

    Update in collection item component

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './collection-item.scss'
-
+import CustomButton from '../custom-button/custom-button'
 const CollectionItem = ({id,name,imageUrl,price}) => {
 return(
     <div className='collection-item'>
@@ -14,6 +14,7 @@ return(
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted> Add to cart </CustomButton>
   </div>
 )
 }
