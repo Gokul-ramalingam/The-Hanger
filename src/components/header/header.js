faa6fe731e7774e2f9f0f2a30462fc81b9792fc8
Gokul <gokulramalingam1@gmail.com>
2019-11-17 15:40:25 +0530

    Update in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,10 @@ import { auth } from '../../firebase/firebase.utils'
 import { connect } from 'react-redux'
 import CartIcon from '../cart-icon/cart-icon'
 import CartDropdown from '../cart-dropdown/cart-dropdown'
+import { createStructuredSelector } from 'reselect'
 import './header.scss';
+import { selectCartHidden } from '../../redux/cart/cart.selector'
+import { selectCurrentUser } from '../../redux/user/user.selector';
 
 const Header = ({ currentUser,hidden }) =>{
     return(
@@ -31,9 +34,9 @@ const Header = ({ currentUser,hidden }) =>{
     )
 }
 
-const mapStateToProps =({user:{currentUser}, cart : { hidden }})=>({
-currentUser,
-hidden
+const mapStateToProps =createStructuredSelector({
+currentUser:selectCurrentUser,
+hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
