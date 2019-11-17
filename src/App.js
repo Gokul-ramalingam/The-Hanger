84a03d6ac0792871fd31b84b9c2b791fd9d2cb27
Gokul <gokulramalingam1@gmail.com>
2019-11-17 15:39:08 +0530

    Update in App component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Homepage from './pages/Homepage/Homepage';
 import Shop from './pages/Shop/Shop'
 import Header from '../src/components/header/header'
 import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp'
+import Checkout from './pages/Checkout/checkout'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 import { setCurrentUser } from './redux/user/user.action'
+import { selectCurrentUser } from './redux/user/user.selector';
 
 class App extends Component{
  unsubscribeFromAuth = null;
@@ -44,6 +47,7 @@ class App extends Component{
     <Switch>
      <Route exact path="/" component={Homepage}/>
      <Route path="/shop" component={Shop}/>
+     <Route exact path='/checkout' component={ Checkout } />
      <Route exact path="/signin" render={() => this.props.currentUser?(<Redirect to='/' />):(<SignInAndSignUp/>)}/>
      </Switch>
     </div>
@@ -51,8 +55,8 @@ class App extends Component{
 }
 }
 
-const mapStateToProps = ({user}) => ({
-currentUser:user.currentUser
+const mapStateToProps = createStructuredSelector({
+currentUser:selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch =>({
