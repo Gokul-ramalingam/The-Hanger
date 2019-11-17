f940e649ef9a47b50156a20dcbed2c0d6cd124ed
Gokul <gokulramalingam1@gmail.com>
2019-11-17 15:40:13 +0530

    Update in cart-icon

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/11.2 shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.action';
+import { createStructuredSelector } from 'reselect'
 import './cart-icon.scss';
+import { selectCartItem } from '../../redux/cart/cart.selector';
 
 const CartIcon = ({ toggleCartHidden,itemCount }) => (
         <div className='cart-icon' onClick={toggleCartHidden}>
@@ -15,8 +17,8 @@ const mapDispatchToProps = dispatch =>({
         toggleCartHidden : () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = ({ cart : {cartItem} }) =>({
-        itemCount : cartItem.reduce((a,b)=>a+b.quantity,0)
+const mapStateToProps =createStructuredSelector({
+        itemCount : selectCartItem
 })
 
 export default connect(
